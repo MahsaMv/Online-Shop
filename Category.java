de2c42c6191dca3186a6a772b957211a1306e58f
MahsaMv <156520622+MahsaMv@users.noreply.github.com>
2024-07-06 14:51:46 +0330

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
-
 public class Category{
-    private String name;
-    private ArrayList<String> products;
-
+    public String name;
+    public ArrayList<Product> products;
     public Category(String name) {
         this.name = name;
         this.products = new ArrayList<>();
@@ -11,12 +9,18 @@ public class Category{
     public String getName() {
         return name;
     }
-    public ArrayList<String> getProducts() {
+    public ArrayList<Product> getProducts() {
         return products;
     }
-
-    public void addProduct(String product) {
+    public void addProduct(Product product) {
         products.add(product);
-
+    }
+    public static Category findCategory(String name){
+        for (Category category : Shop.categories){
+            if (category.name.equals(name)){
+                return category;
+            }
+        }
+        return null;
     }
 }
