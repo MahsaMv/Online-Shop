8f5ec842789e8af152b455db5348dcf2008d4639
MahsaMv <156520622+MahsaMv@users.noreply.github.com>
2024-07-06 14:54:29 +0330

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
-
 public class Product {
-    private String name;
-    private double price;
-    private int inventory;
+    public String name;
+    public double price;
+    public int inventory;
     private ArrayList<String> comments;
-    private Category category;
+    public Category category;
 
-    public Product(String name, double price, int inventory, ArrayList<String> comments, Category category) {
+    public Product(String name, double price, int inventory, Category category) {
         this.name = name;
-        this.price = 0.0;
-        this.inventory = 0;
+        this.price = price;
+        this.inventory = inventory;
         this.comments = new ArrayList<>();
         this.category = category;
     }
@@ -27,18 +26,18 @@ public class Product {
         return comments;
     }
     public void displayProduct() {
-        System.out.println("entity.Product Name: " + name);
+        System.out.println("Product Name: " + name);
         System.out.println("Price: " + "$" + price);
         System.out.println("Quantity: " + inventory);
         System.out.println("Comments: " + comments);
-        System.out.println("entity.Category: " + category.getName());
+        System.out.println("Category: " + category.getName());
     }
 
     public void reduceQuantity(int amount) {
         if (amount <= inventory)
             inventory -= amount;
     }
-
+//TODO write this
     public void addComment(String comment) {
         comments.add(comment);
     }
